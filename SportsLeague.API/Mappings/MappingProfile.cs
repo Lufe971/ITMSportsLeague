6a1600a5f4a4712fad75bd1a1cd8023d0edb897e
Lufe971 <luisafer910@hotmail.com>
2026-04-11 12:33:28 -0500

    Agregar archivos de proyecto.

---
+++ SportsLeague.API/Mappings/MappingProfile.cs
@@ -0,0 +1,13 @@
+﻿namespace SportsLeague.API.Mappings
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            // Team mappings
+            CreateMap<TeamRequestDTO, Team>();
+            CreateMap<Team, TeamResponseDTO>();
+        }
+    }
+
+}
---
+++ SportsLeague.DataAccess/Repositories/TeamRepository.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SportsLeague.DataAccess.Context;
+using SportsLeague.Domain.Entities;
+using SportsLeague.Domain.Interfaces.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportsLeague.DataAccess.Repositories
+{
+    public class TeamRepository : GenericRepository<Team>, ITeamRepository
+    {
+        public TeamRepository(LeagueDbContext context) : base(context)
+        {
+        }
+
+        public async Task<Team?> GetByNameAsync(string name)
+        {
+            return await _dbSet
+                .FirstOrDefaultAsync(t => t.Name.ToLower() == name.ToLower());
+        }
+
+        public async Task<IEnumerable<Team>> GetByCityAsync(string city)
+        {
+            return await _dbSet
+                .Where(t => t.City.ToLower() == city.ToLower())
+                .ToListAsync();
+        }
+    }
+
+}
